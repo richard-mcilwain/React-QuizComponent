730e03ea460d77e851b21f7918c7eb4db64a3ca0
Richard McIlwain <richard-g-mcilwain@users.noreply.github.com>
2020-04-17 04:13:29 -0500

    Update list item formatting in QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 class QuizQuestion extends Component {
-
   constructor(props) {
     super(props);
 
@@ -11,15 +10,15 @@ class QuizQuestion extends Component {
   render() {
     return (
       <main>
-      <section>
-        <p>{ this.props.quiz_question.instruction_text }</p>
-      </section>
-      <section className="buttons">
-        <ul>
-          { this.props.quiz_question.answer_options[0] }
-        </ul>
-      </section>
-    </main>
+        <section>
+          <p>{this.props.quiz_question.instruction_text}</p>
+        </section>
+        <section className="buttons">
+          <ul>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
+          </ul>
+        </section>
+      </main>
     );
   }
 }
