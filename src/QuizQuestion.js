d9ca0f186bee7f82e712f4599f30de6e0b889128
Richard McIlwain <richard-g-mcilwain@users.noreply.github.com>
2020-04-17 05:44:18 -0500

    Fix incorrect_answer state management to reset the value on correct answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -40,6 +40,7 @@ class QuizQuestion extends Component {
 
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
+      this.setState({ incorrect_answer: false });
       this.props.showNextQuestionHandler();
     } else {
       this.setState({ incorrect_answer: true });
