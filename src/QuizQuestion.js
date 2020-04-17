53e2e402c085890a9a99d955ae75f590659cda71
Richard McIlwain <richard-g-mcilwain@users.noreply.github.com>
2020-04-17 05:41:15 -0500

    Add incorrect_answer state management

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,7 +5,10 @@ class QuizQuestion extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { quiz_position: 1 };
+    this.state = {
+      quiz_position: 1,
+      incorrect_answer: false,
+    };
   }
 
   render() {
@@ -28,6 +31,9 @@ class QuizQuestion extends Component {
         <section className="buttons">
           <ul>{answerOptions}</ul>
         </section>
+        {this.state.incorrect_answer === true ? (
+          <p className="error">Sorry, that's not right.</p>
+        ) : null}
       </main>
     );
   }
@@ -35,6 +41,8 @@ class QuizQuestion extends Component {
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
       this.props.showNextQuestionHandler();
+    } else {
+      this.setState({ incorrect_answer: true });
     }
   }
 }
