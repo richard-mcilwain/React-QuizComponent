8b057a87de95b41ab49129bcb4ce3f3e0a8577c4
Richard McIlwain <richard-g-mcilwain@users.noreply.github.com>
2020-04-17 05:06:51 -0500

    Add clickHandlers

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,9 +20,16 @@ class Quiz extends Component {
       return (
         <QuizQuestion
           quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+          showNextQuestionHandler={this.showNextQuestion.bind(this)}
         />
       );
     }
+  }
+
+  showNextQuestion() {
+    this.setState({
+      quiz_position: this.state.quiz_position + 1,
+    });
   }
 }
 
--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -9,20 +9,35 @@ class QuizQuestion extends Component {
   }
 
   render() {
+    const answerOptions = this.props.quiz_question.answer_options.map(
+      (option, index) => {
+        return (
+          <QuizQuestionButton
+            key={index}
+            button_text={option}
+            clickHandler={this.handleClick.bind(this)}
+          >
+            option
+          </QuizQuestionButton>
+        );
+      }
+    );
     return (
       <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
-          <ul>
-            <QuizQuestionButton
-              button_text={this.props.quiz_question.answer_options[0]}
-            />
-          </ul>
+          <ul>{answerOptions}</ul>
         </section>
       </main>
     );
+  }
+
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler();
+    }
   }
 }
 
